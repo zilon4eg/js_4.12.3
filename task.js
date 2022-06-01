5d79fce064ade168d7a2d72279b4f41dd087008a
zilon4eg <hdd500t@gmail.com>
2022-06-01 17:50:44 +0300

    work on bugs + logout

--- task.js
+++ task.js
@@ -1,30 +1,29 @@
 function authSuccess(userId) {
+    localStorage.setItem('user', userId);
     document.getElementById('signin').classList.remove('signin_active');
     const welcomeMsg = document.getElementById('user_id');
     welcomeMsg.textContent = userId;
     document.getElementById('welcome').classList.add('welcome_active');
 }
 
-function clearAuthForm() {
-    const entryFields = authForm.querySelectorAll('.control');
-    for (const entryField of entryFields) {
-        entryField.value = '';
-    }
+
+function logout() {
+    localStorage.removeItem('user');
+    document.getElementById('signin').classList.add('signin_active');
+    document.getElementById('welcome').classList.remove('welcome_active');
 }
 
 
 const authForm = document.getElementById('signin__form');
+
+const userIdLocal = localStorage.getItem('user');
+if (userIdLocal != null) {
+    authSuccess(userIdLocal);
+}
+
 authForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let xhr = new XMLHttpRequest();
-
-    xhr.onload = () => {
-        if (xhr.status != 200) { // HTTP ошибка?
-          // обработаем ошибку
-          alert('Ошибка: ' + xhr.status);
-          return;
-        }
-    }
 
     xhr.onload = () => {
         const postRes = JSON.parse(xhr.responseText);
@@ -35,11 +34,16 @@ authForm.addEventListener('submit', (e) => {
         }
         else {
             alert('Неверный логин/пароль!');
-            clearAuthForm();
+            authForm.reset();
         }
     };
 
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
     const authFormData = new FormData(document.getElementById('signin__form'));
     xhr.send(authFormData);
+});
+
+const logoutBtn = document.getElementById('signout__btn');
+logoutBtn.addEventListener('click', () => {
+    logout();
 });
